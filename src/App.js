8b6a8cfc6607560c5467eda2e3e39c5a35c909d9
Enes Baspinar <enesbaspinar07@gmail.com>
2021-07-18 10:14:30 +0300

    Add completed project files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import React from "react";
+import AddComment from "./components/AddComment";
+import Comment from "./components/Comment";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentList: [],
+    };
+  }
+
+  handleSetCommentList = (commentList) => {
+    this.setState({ commentList });
+  };
+
+  render() {
+    let commentList = this.state.commentList;
+    return (
+      <div className="App">
+        <AddComment
+          className="add-comment-div"
+          commentList={commentList}
+          setCommentList={this.handleSetCommentList}
+        />
+        {!commentList.length ? (
+          <span>Hiç yorum yok.</span>
+        ) : (
+          <Comment className="comment-div" commentList={commentList} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
